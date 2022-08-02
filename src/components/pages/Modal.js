8d54508d7beb148807ede8f700232f558fb36225
Md. Saiful Isalm <50217204+saifulislamsappu@users.noreply.github.com>
2022-08-02 23:45:24 +0600

    update modal.js

--- src/components/pages/Modal.js
+++ src/components/pages/Modal.js
@@ -10,7 +10,7 @@ const Modal = ({setModalOpen}) => {
         <div className="modal">
             <div class="modal__content">
                 <h3>পর্যাপ্ত পয়েন্ট নেই</h3>
-                <img src="images/modal-image.png"/>
+                <img src="images/modal-image.png" alt="quiz app"/>
                 <p>রিয়ার্ড সংগ্রহ করার জন্য আপনার পয়েন্ট নেই।  নিয়মিত কুইজাস গেম খেলে পয়েন্ট অর্জন করুন </p>
                 <div className="top-layout"></div>
             </div>
