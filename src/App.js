6baa3996e0a8f657c520c740a158a1b17bed91c3
Md. Saiful Isalm <50217204+saifulislamsappu@users.noreply.github.com>
2022-08-02 23:22:46 +0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './components/pages/Home';
-import Slider from './components/pages/Slider';
-import Category from './components/pages/Category';
-import Navbar from './components/layout/Navbar';
 import Rewards from './components/pages/Rewards';
-import Error from './components/pages/Error';
 function App(props) {
   return (
       <div className="App">
